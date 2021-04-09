12a8f4ca6834d527f1e51ed9fdf568d3d6c7284b
Satoshi Ogata <satosystems@gmail.com>
2021-04-10 00:14:53 +0900

    Change behavior of this script
    以下のように動作を修正。
    
    - レーダーチャートを別のスプレッドシートに作成
    - 作成したスプレッドシートの編集者に「編集者」シートに記載されているメールアドレスを追加
    - 作成したスプレッドシートへのリンクを「リンク」シートに追加
    - レーダーチャートの最大値と最小値を設定
    - レーダーチャートのラインを直線に変更
    
    新規に作成するスプレッドシートはドライブの「スキルチェックシート」内に作成され、同一ファイルが存在する場合は古いファイルはゴミ箱に移動される。

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-/* global Charts, SpreadsheetApp */
+/* global Charts, DriveApp, Logger, MimeType, SpreadsheetApp */
 
 'use strict'
 
@@ -16,16 +16,43 @@ function onOpen () {
 }
 
 /**
- * スプレッドシートにレーダーチャートのシートを追加する。
+ * 指定されたファイルに対してスプレッドシートの「編集者」シートに記載されている編集者をまとめて追加する。
  *
- * @param {Spreadsheet} ss 現在操作しているスプレッドシート
+ * @param {Object} GAS のファイルオブジェクト
+ */
+function addEditors (file) {
+  const emailAddresses = SpreadsheetApp.getActiveSpreadsheet()
+    .getSheetByName('編集者')
+    .getDataRange()
+    .getValues()
+    .filter(row => row[0] !== '')
+    .map(row => row[0])
+  if (emailAddresses.length !== 0) {
+    file.addEditors(emailAddresses)
+  }
+}
+
+/**
+ * ハイパーリンク文字列を作成する。
+ *
+ * @param {String} title タイトル
+ * @param {String} url URL
+ * @returns {String} ハイパーリンク文字列
+ */
+const makeLinkString = (title, url) => `=HYPERLINK("${url}","${title}")`
+
+/**
+ * 新しく作成したスプレッドシートにレーダーチャートのシートを追加する。
+ *
  * @param {Array.<String>} header 整形済み項目のヘッダ
  * @param {Object} categories スキルカテゴリ一覧
  * @param {Array.<String>} record レーダーチャートを作成したい対象者の情報
+ * @returns {Array.<String>|null} 追加・削除されたスプレッドシートの URL。
+ * 追加された URL が先頭、削除された URL がそれ以降。何もしなかった場合は null
  */
-function create (ss, header, categories, record) {
+function create (header, categories, record) {
   if (record.length === 1) {
-    return
+    return null
   }
   const values = []
   for (let i = 0; i < record.length && record[i] !== ''; i++) {
@@ -33,27 +60,44 @@ function create (ss, header, categories, record) {
     values[i] = [header[i], record[i]]
   }
   // シート名は 100 文字まで
-  const sheetName = `${values[1][1]} ${values[2][1]}`.substring(0, 100)
-  const oldSheet = ss.getSheetByName(sheetName)
-  if (oldSheet) {
-    ss.deleteSheet(oldSheet)
+  const spreadSheetName = `${values[1][1]} ${values[2][1]}`.substring(0, 100)
+  const files = DriveApp.searchFiles(`title = '${spreadSheetName}' and trashed = false and mimeType = '${MimeType.GOOGLE_SHEETS}'`)
+  const links = []
+  while (files.hasNext()) {
+    const file = files.next()
+    links.push(makeLinkString(spreadSheetName, file.getUrl()))
+    file.setTrashed(true) // 古いスプレッドシートは削除
   }
-  const insertedSheet = ss.insertSheet()
-  insertedSheet.setName(sheetName)
-  insertedSheet.getRange(1, 1, values.length, values[0].length).setValues(values)
+  // 新しいスプレッドシートを作成
+  const ss = SpreadsheetApp.create(spreadSheetName)
+  const file = DriveApp.getFileById(ss.getId())
+  addEditors(file)
+  const folderName = 'スキルチェックシート'
+  const folders = DriveApp.getFoldersByName(folderName)
+  const folder = folders.hasNext() ? folders.next() : DriveApp.createFolder(folderName)
+  file.moveTo(folder)
+  const s = ss.getActiveSheet()
+  s.getRange(1, 1, values.length, values[0].length).setValues(values)
 
   let row = 4 // レーダーチャートを配置する開始位置
   for (const key in categories) {
     const category = categories[key]
-    const range = insertedSheet.getRange(category.offset, 1, category.count, 2)
-    const chart = insertedSheet.newChart()
+    const range = s.getRange(category.offset, 1, category.count, 2)
+    const chart = s.newChart()
       .addRange(range)
       .setChartType(Charts.ChartType.RADAR)
       .setPosition(row, 3, 0, 0)
+      // 平滑線
+      .setOption('smoothLine', false)
       .setOption('title', key)
-    insertedSheet.insertChart(chart.build())
+      .setOption('curveType', 'none')
+      .setOption('vAxis.minValue', 0)
+      .setOption('vAxis.maxValue', 5)
+    s.insertChart(chart.build())
     row += 18 // レーダーチャートの大きさを考慮した値
   }
+  links.unshift(makeLinkString(spreadSheetName, ss.getUrl()))
+  return links
 }
 
 /**
@@ -93,8 +137,52 @@ function createRadarChart () {
   const dataSheet = ss.getSheets()[0]
   const header = dataSheet.getRange(1, 1, 1, 256).getValues()[0]
   const records = ss.getActiveRangeList().getRanges().reduce((acc, cur) => acc.concat(cur.getValues()), [])
+  Logger.log(`header: ${header}`)
+  Logger.log(`records: ${records}`)
   const { newHeader, categories } = parseHeader(header)
-  records.forEach(record => create(ss, newHeader, categories, record))
+  const allLinks = records.map(record => create(newHeader, categories, record)).filter(v => v !== null)
+  if (allLinks.length === 0) {
+    return
+  }
+  const linkSheetName = 'リンク'
+  const createSheet = () => {
+    const s = ss.insertSheet()
+    s.setName(linkSheetName)
+    ss.moveActiveSheet(2) // 2 番目に移動
+    return s
+  }
+  const s = ss.getSheetByName(linkSheetName) || createSheet()
+  let rows = s.getDataRange()
+    .getRichTextValues()
+    .map(row => {
+      const text = row[0].getText()
+      const url = row[0].getLinkUrl()
+      const newRow = [makeLinkString(text, url)]
+      newRow.text = text
+      return newRow
+    })
+    .filter(row => row[0] !== '' && row[0] !== '=HYPERLINK("null","")')
+  allLinks.forEach(links => {
+    if (links.length > 1) {
+      const sublinks = links.slice(1)
+      const filter = row => {
+        for (const link of sublinks) {
+          if (link.includes(`"${row.text}"`)) {
+            return false
+          }
+        }
+        return true
+      }
+      rows = rows.filter(filter)
+      rows.push([links[0]])
+    } else {
+      rows.push([links[0]])
+    }
+  })
+  s.clear()
+  if (rows.length !== 0) {
+    s.getRange(1, 1, rows.length).setValues(rows)
+  }
 }
 
 // eslint-disable-next-line no-unused-vars
